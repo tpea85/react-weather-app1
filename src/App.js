b597d54af44b9fe6c94e34de569546e103a6faaa
Talancia Pea <every1moves@gmail.com>
2024-02-09 13:50:34 -0500

    Added Bootstrap and H1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 export default function App() {
   return (
   <div className="App">
+    <div className="container">
    <h1>Weather App </h1>
     <footer>
 This project was coded by Talancia Pea and is {""}
@@ -12,6 +13,7 @@ This project was coded by Talancia Pea and is {""}
     target="_blank"
     >open-sourced on GitHub.</a>
     </footer> 
+  </div>
   </div>
   );
 }
