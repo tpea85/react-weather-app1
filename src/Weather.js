4b1b5c4a2076fda96f37a30c789aeda164762af8
Talancia Pea <every1moves@gmail.com>
2024-02-09 16:56:56 -0500

    basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -10,10 +10,11 @@ return (
         <div className="col-9">
         <input type="search" placeholder="Enter a city..."
     className="form-control"
+    autoFocus="on"
     />
     </div>
     <div className="col-3">
-    <input type="submit" value="Search" className="btn btn-primary" />
+    <input type="submit" value="Search" className="btn btn-primary w-100" />
     </div>
     </div>
     </form>
@@ -22,11 +23,18 @@ return (
        <li>Wednesday 07:00</li>
        <li>Mostly Cloudy</li> 
     </ul>
-    <div className="row">
+    <div className="row mt-3">
         <div className="col-6">
+    <div className="clearfix">
            <img 
-           src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Partly Cloudy"/> 
-        6°C
+           src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+           alt="Partly Cloudy"
+           /> 
+          <div className="float-left">
+        <span className="temperature">6</span>
+        <span className="unit">°C</span>
+        </div>
+        </div>
         </div>
         <div className="col-6">
             <ul>
