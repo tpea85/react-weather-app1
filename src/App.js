051bef04c1c916cef9a721c991b9058a5c52a2ec
Talancia Pea <every1moves@gmail.com>
2024-02-09 13:26:43 -0500

    GitHub link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+  <div className="App">
+   <h1>Weather App </h1>
+    <footer>
+This project was coded by Talancia Pea and is {""}
+    <a 
+    href="https://github.com/tpea85/react-weather-app1/tree/master"
+    target="_blank"
+    >open-sourced on GitHub.</a>
+    </footer> 
+  </div>
+  );
 }
 
 
