2aa8a10191bc0496666ebe403e422467531c0a01
Talancia Pea <every1moves@gmail.com>
2024-02-16 16:44:50 -0500

    Added Celsius and Fahrenheit Conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return  (
@@ -19,9 +20,9 @@ export default function WeatherInfo(props) {
         <div>
         <WeatherIcon code={props.data.icon} />
         </div>
-          <div className="float-left">
-        <span className="temperature">{Math.round(props.data.temperature)}</span>
-        <span className="unit">°C</span>
+
+          <div>
+            <WeatherTemperature celsius={props.data.temperature} />
         </div>
         </div>
         </div>
